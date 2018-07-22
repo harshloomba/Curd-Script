08f872d530d0d5db08029e1aa1aba0db51190672
unknown <hloomba@gmu.edu>
2018-07-21 20:11:24 -0400

    added not null contraint on the attribute

--- model.py
+++ model.py
@@ -7,9 +7,9 @@ class Product(db.Model):
     ind = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(120),primary_key=True)
     datetime = db.Column(db.String(120), primary_key=True)
-    longitude = db.Column(db.Integer, unique=False)
-    latitude = db.Column(db.Integer, unique=False)
-    elevation = db.Column(db.Integer, unique=False)
+    longitude = db.Column(db.Integer, unique=False, nullable=False)
+    latitude = db.Column(db.Integer, unique=False, nullable=False)
+    elevation = db.Column(db.Integer, unique=False, nullable=False)
 
     def __init__(self, ind, description, datetime, longitude, latitude, elevation):
         self.ind = ind
